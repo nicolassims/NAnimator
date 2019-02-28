ad119ba3997035922d97a056f7b2d777c5f1abbc
nicolassims <sableye.nico@gmail.com>
2019-02-28 16:11:12 -0500

    Adding MotionImpl name.

--- src/model/MotionImpl.java
+++ src/model/MotionImpl.java
@@ -3,22 +3,24 @@ package model;
 public class MotionImpl implements Motion {
   private int startTick;
   private int endTick;
+  private String name;
   private Keyframe startFrame;
   private Keyframe endFrame;
 
-  public MotionImpl(int startTick, int endTick, Keyframe startFrame, Keyframe endFrame) {
+  public MotionImpl(int startTick, int endTick, String name, Keyframe startFrame, Keyframe endFrame) {
     if (startTick < 0 || startTick >= endTick || startFrame == null || endFrame == null) {
       throw new IllegalArgumentException("MotionImpl not able to construct properly.");
     }
     this.startTick = startTick;
     this.endTick = endTick;
+    this.name = name;
     this.startFrame = startFrame;
     this.endFrame = endFrame;
   }
 
   @Override
   public String toFile() {
-    return "motion " + startTick + " " + startFrame.toFile()
+    return "motion " + name + " " + startTick + " " + startFrame.toFile()
         + " " + endTick + " " + endFrame.toFile();
   }
 }
