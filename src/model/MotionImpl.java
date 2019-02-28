c813cddda6f0ca9c57477b7206d680b4319dcc94
nicolassims <sableye.nico@gmail.com>
2019-02-28 16:17:23 -0500

    Adding MotionImpl comments.

--- src/model/MotionImpl.java
+++ src/model/MotionImpl.java
@@ -1,14 +1,31 @@
 package model;
 
+/**
+ * This class represents a motion affecting one shape across some range of ticks.
+ */
 public class MotionImpl implements Motion {
+
   private int startTick;
   private int endTick;
   private String name;
   private Keyframe startFrame;
   private Keyframe endFrame;
 
-  public MotionImpl(int startTick, int endTick, String name, Keyframe startFrame, Keyframe endFrame) {
-    if (startTick < 0 || startTick >= endTick || startFrame == null || endFrame == null) {
+  /**
+   * Initializes MotionImpl, given two ints, a string, and two Keyframes.
+   *
+   * @param startTick The first tick this motion is active on.
+   * @param endTick The last tick this motion is active on.
+   * @param name The name of this motion.
+   * @param startFrame The state of this motion on its first frame.
+   * @param endFrame The state of this motion on its last frame.
+   * @throws IllegalArgumentException if the tick measurements are impossible, or if any of the
+   * constructor's variables are null.
+   */
+  public MotionImpl(int startTick, int endTick, String name, Keyframe startFrame,
+      Keyframe endFrame) {
+    if (startTick < 0 || startTick >= endTick ||
+        startFrame == null || endFrame == null || name == null) {
       throw new IllegalArgumentException("MotionImpl not able to construct properly.");
     }
     this.startTick = startTick;
