8b103dede5b00c81ba520f8c35dc8d4ef77d9e8d
nicolassims <sableye.nico@gmail.com>
2019-02-28 17:00:22 -0500

    Removed string name and replaced with reference to parent.

--- src/model/MotionImpl.java
+++ src/model/MotionImpl.java
@@ -7,7 +7,7 @@ public class MotionImpl implements Motion {
 
   private int startTick;
   private int endTick;
-  private String name;
+  private Shape parent;
   private Keyframe startFrame;
   private Keyframe endFrame;
 
@@ -16,28 +16,27 @@ public class MotionImpl implements Motion {
    *
    * @param startTick The first tick this motion is active on.
    * @param endTick The last tick this motion is active on.
-   * @param name The name of this motion.
    * @param startFrame The state of this motion on its first frame.
    * @param endFrame The state of this motion on its last frame.
    * @throws IllegalArgumentException if the tick measurements are impossible, or if any of the
    * constructor's variables are null.
    */
-  public MotionImpl(int startTick, int endTick, String name, Keyframe startFrame,
-      Keyframe endFrame) {
-    if (startTick < 0 || startTick >= endTick ||
-        startFrame == null || endFrame == null || name == null) {
+  public MotionImpl(int startTick, int endTick, Shape parent,
+      Keyframe startFrame, Keyframe endFrame) {
+    if (startTick < 0 || startTick >= endTick || parent == null
+        || startFrame == null || endFrame == null) {
       throw new IllegalArgumentException("MotionImpl not able to construct properly.");
     }
     this.startTick = startTick;
     this.endTick = endTick;
-    this.name = name;
+    this.parent = parent;
     this.startFrame = startFrame;
     this.endFrame = endFrame;
   }
 
   @Override
   public String toFile() {
-    return "motion " + name + " " + startTick + " " + startFrame.toFile()
+    return "motion " + parent.getName() + " " + startTick + " " + startFrame.toFile()
         + " " + endTick + " " + endFrame.toFile();
   }
 }
