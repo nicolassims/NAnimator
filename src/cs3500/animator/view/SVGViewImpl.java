5870f5d1afe3908c9a4bc8b8894eec7d2200c19e
nicolassims <sableye.nico@gmail.com>
2019-03-14 16:17:53 -0400

    Working on implementing the SVG changes. Got the intitial bracket made.

--- src/cs3500/animator/view/SVGViewImpl.java
+++ src/cs3500/animator/view/SVGViewImpl.java
@@ -8,34 +8,24 @@ public class SVGViewImpl implements View {
 
   @Override
   public void displayView(Animation model) {
-    StringBuilder viewString = new StringBuilder("<svg width=\"700\" height=\"500\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n");
+    StringBuilder viewString = new StringBuilder(
+        "<svg width=\"700\" height=\"500\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n");
     Map<String, Shape> shapes = model.getShapes();
     for (Shape shape : shapes.values()) {
       if (shape.getShape().equals("rectangle")) {
-        viewString.append("<rect");
+        viewString.append("<rect").append(" id=\"").append(shape.getName()).append("\" x=\"")
+            .append(shape.getFirstX()).append("\" y=\"").append(shape.getFirstY()).append("\"")
+            .append(" width=\"").append(shape.getFirstWidth()).append("\" height=\"");
       } else if (shape.getShape().equals("ellipse")) {
-        viewString.append("<ellipse");
+        viewString.append("<ellipse").append(" id=\"").append(shape.getName()).append("\" cx=\"")
+            .append(shape.getFirstX()).append("\" cy=\"").append(shape.getFirstY()).append("\"")
+            .append(" rx=\"").append(shape.getFirstWidth()).append("\" ry=\"");
       } else {
         throw new IllegalArgumentException("This shape is not a rectangle or an ellipse.");
       }
-      viewString.append(" id=\"" + shape.getName() +"\" x=\"" + shape.getFirstX() + "\" y=\"" + shape.getFirstY() + "\"");
-      viewString.append(" width=\"" + shape.getFirstWidth() + "\" height=\"" + shape.getFirstHeight() + "\"");
-      viewString.append(" fill=\"rgb(" + shape.getFirstColors() + ")\" visibility=\"visible\""); //alter this if not visible on first frame
+      viewString.append(shape.getFirstHeight()).append("\"").append(" fill=\"rgb(")
+          .append(shape.getFirstColors()).append(")\" visibility=\"")
+          .append(shape.getFirstTick() == 0 ? "visible" : "hidden").append("\" >");
     }
-    //<rect id="P" x="200" y="200" width="50" height="100" fill="rgb(128,0,128)" visibility="visible" >
-
-
-    /*String[] modelString = model.toString().split(",");
-    for (int i = 0; i < modelString.length; i++) {
-      if (modelString[i].equals("shape")) {
-        if (modelString[i].equals("rectangle")) {
-          viewString.append()
-        } else if (modelString[i].equals("ellipse")) {
-
-        } else {
-          throw new IllegalArgumentException("this shape is not a rectangle or an ellipse.");
-        }
-      }
-    }*/
   }
 }
