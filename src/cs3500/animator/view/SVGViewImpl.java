b988eeac683ea65f96f281de9cbf85b2113947f4
nicolassims <sableye.nico@gmail.com>
2019-03-14 15:00:34 -0400

    Working on SVG View.

--- src/cs3500/animator/view/SVGViewImpl.java
+++ src/cs3500/animator/view/SVGViewImpl.java
@@ -6,6 +6,8 @@ public class SVGViewImpl implements View {
 
   @Override
   public void displayView(Animation model) {
+    StringBuilder anim = new StringBuilder();
+    anim.append("<svg width=\"700\" height=\"500\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">");
 
   }
 }
