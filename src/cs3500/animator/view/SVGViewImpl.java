fec5ca013dea6c7b93b1399cd16ac080b93fb6b7
nicolassims <sableye.nico@gmail.com>
2019-03-14 17:23:18 -0400

    I'm some kind of god of append chaining. (Large changes to SVGViewImpl, namely consolidated most opening bracket code into one block.)

--- src/cs3500/animator/view/SVGViewImpl.java
+++ src/cs3500/animator/view/SVGViewImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class SVGViewImpl implements View {
+
   private int ticksPerSecond = 60;
 
   @Override
@@ -17,7 +18,11 @@ public class SVGViewImpl implements View {
 
   @Override
   public void displayView(Animation model) {
-    String endtag = "";
+    String endtag;
+    String xname = "x";
+    String yname = "y";
+    String widthname = "width";
+    String heightname = "height";
     StringBuilder viewString = new StringBuilder(
         "<svg width=\"700\" height=\"500\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n");
     Map<String, Shape> shapes = model.getShapes();
@@ -25,35 +30,47 @@ public class SVGViewImpl implements View {
     for (Shape shape : shapes.values()) {
       if (shape.getShape().equals("rectangle")) {
         endtag = "</rect>\n";
-        viewString.append("<rect").append(" id=\"").append(shape.getName()).append("\" x=\"")
-            .append(shape.getFirstX()).append("\" y=\"").append(shape.getFirstY()).append("\"")
-            .append(" width=\"").append(shape.getFirstWidth()).append("\" height=\"");
       } else if (shape.getShape().equals("ellipse")) {
         endtag = "</ellipse>\n";
-        viewString.append("<ellipse").append(" id=\"").append(shape.getName()).append("\" cx=\"")
-            .append(shape.getFirstX()).append("\" cy=\"").append(shape.getFirstY()).append("\"")
-            .append(" rx=\"").append(shape.getFirstWidth()).append("\" ry=\"");
+        xname = "cx";
+        yname = "cy";
+        widthname = "rx";
+        heightname = "ry";
       } else {
         throw new IllegalArgumentException("This shape is not a rectangle or an ellipse.");
       }
-      viewString.append(shape.getFirstHeight()).append("\"").append(" fill=\"rgb(")
+      viewString.append("<rect").append(" id=\"").append(shape.getName()).append("\" ")
+          .append(xname).append("=\"")
+          .append(shape.getFirstX()).append("\" ").append(yname).append("=\"")
+          .append(shape.getFirstY()).append("\"").append(" ").append(widthname).append("=\"")
+          .append(shape.getFirstWidth()).append("\" ")
+          .append(heightname).append("=\"").append(shape.getFirstHeight()).append("\"")
+          .append(" fill=\"rgb(")
           .append(shape.getFirstColors()).append(")\" visibility=\"")
           .append(shape.getFirstTick() == 0 ? "visible" : "hidden").append("\" >\n");
 
       List<Motion> motions = shape.getMotions();
       for (Motion motion : motions) {
         String[] motionArray = motion.toFile().split(" ");
-        viewString.append("<animate attributeType=\"xml\" begin=\"" + motion.getStartFrame().getTick() * ticksPerSecond + "\""
-            + " dur=\"" + motion.getEndFrame().getTick() * ticksPerSecond + "\"\n"); //add attributeName, from, to, fill
+        String attributeName;
+        viewString.append("<animate attributeType=\"xml\" begin=\""
+            + motion.getStartFrame().getTick() * ticksPerSecond + "\""
+            + " dur=\"" + motion.getEndFrame().getTick() * ticksPerSecond
+            + "\"\n"); //add attributeName, from, to, fill
+        for (int i = 3; i < 9; i++) {
+          if (!motionArray[i].equals(motionArray[i + 8])) {
+            if (i == 3)
+          }
+        }
+
       }
+      //       name time x   y  w  h  r   g b time x   y  w  h  r   g  b
+      //motion S0   0    100 75 20 15 255 0 0 0    100 75 20 15 255 0  0
+      //     0 1    2    3   4  5  6  7   8 9 10   11  12 13 14 15  16 17
+      //3 > 11
+      //9 > 17
 
-
-    //<animate attributeType="xml" begin="2000.0ms" dur="5000.0ms" attributeName="cx" from="500" to="600" fill="remove" />
-
-
-
-
-
+      //<animate attributeType="xml" begin="2000.0ms" dur="5000.0ms" attributeName="cx" from="500" to="600" fill="remove" />
 
       viewString.append(endtag);
 
