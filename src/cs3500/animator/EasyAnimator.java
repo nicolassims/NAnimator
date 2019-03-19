450cf1922f6c4790a580f0d62f6cb707a2e0e71a
nicolassims <sableye.nico@gmail.com>
2019-03-19 16:01:51 -0400

    Java Style updates 2.

--- src/cs3500/animator/EasyAnimator.java
+++ src/cs3500/animator/EasyAnimator.java
@@ -12,51 +12,51 @@ import java.io.FileReader;
 
 public final class EasyAnimator {
 
-    public static void main(String[] args) {
-        String inArg = "";
-        String viewArg = "";
-        float speedArg = 1;
-        String outArg = "System.out";
+  public static void main(String[] args) {
+    String inArg = "";
+    String viewArg = "";
+    float speedArg = 1;
+    String outArg = "System.out";
 
-        for (int i = 0; i < args.length; i += 2) {
-            String command = args[i];
-            String value = args[i + 1];
+    for (int i = 0; i < args.length; i += 2) {
+      String command = args[i];
+      String value = args[i + 1];
 
-            switch (command) {
-                case "-in":
-                    inArg = value;
-                    break;
-                case "-out":
-                    outArg = value;
-                    break;
-                case "-view":
-                    viewArg = value;
-                    break;
-                case "-speed":
-                    speedArg = Float.parseFloat(value);
-                    break;
-            }
-        }
-
-        if (inArg.equals("")) {
-            throw new IllegalArgumentException("-in not provided");
-        }
-        if (viewArg.equals("")) {
-            throw new IllegalArgumentException("-view not provided");
-        }
-
-        try {
-            new AnimationReader();
-            Animation model = AnimationReader
-                    .parseFile(new FileReader(new File("resources/" + inArg).getAbsolutePath()),
-                            new AnimationImpl.Builder());
-
-            View view = new ViewFactoryImpl().getView(viewArg);
-            view.setTicksPerSecond(speedArg);
-            view.setOutputDestination(outArg);
-            view.displayView(model);
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("File not found: " + inArg);
-        }
+      switch (command) {
+        case "-in":
+          inArg = value;
+          break;
+        case "-out":
+          outArg = value;
+          break;
+        case "-view":
+          viewArg = value;
+          break;
+        case "-speed":
+          speedArg = Float.parseFloat(value);
+          break;
+      }
     }
+
+    if (inArg.equals("")) {
+      throw new IllegalArgumentException("-in not provided");
+    }
+    if (viewArg.equals("")) {
+      throw new IllegalArgumentException("-view not provided");
+    }
+
+    try {
+      new AnimationReader();
+      Animation model = AnimationReader
+          .parseFile(new FileReader(new File("resources/" + inArg).getAbsolutePath()),
+              new AnimationImpl.Builder());
+
+      View view = new ViewFactoryImpl().getView(viewArg);
+      view.setTicksPerSecond(speedArg);
+      view.setOutputDestination(outArg);
+      view.displayView(model);
+    } catch (FileNotFoundException e) {
+      throw new IllegalArgumentException("File not found: " + inArg);
+    }
+  }
 }
