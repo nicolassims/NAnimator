1e32775b8b77c64bec5fbf73b77946de15d39226
nicolassims <sableye.nico@gmail.com>
2019-03-28 15:34:55 -0400

    Made the model of TextualViewImpl do no work.

--- src/cs3500/animator/view/TextualViewImpl.java
+++ src/cs3500/animator/view/TextualViewImpl.java
@@ -3,6 +3,7 @@ package cs3500.animator.view;
 import static cs3500.animator.view.AppendableWriter.writeToDestination;
 
 import cs3500.animator.model.Animation;
+import cs3500.animator.model.Motion;
 import cs3500.animator.model.Shape;
 
 /**
@@ -13,23 +14,47 @@ public class TextualViewImpl implements TextBasedView {
   private Appendable outputDestination;
 
   @Override
+  public void setOutputDestination(Appendable outArg) {
+    this.outputDestination = outArg;
+  }
+
+  @Override
   public void displayView(Animation model) {
     int x = model.getX();
     int y = model.getY();
     int w = model.getCanvasWidth();
     int h = model.getCanvasHeight();
     StringBuilder built = new StringBuilder("");
+    built.append("canvas ").append(x).append(" ").append(y).append(" ").append(w).append(" ")
+        .append(h).append("\n");
     for (Shape s : model.getShapes().values()) {
-      built.append("shape ").append(s.getName()).append(" ").append(s.getShape()).append("\n")
-          .append(s.toFile());
+      built.append("shape ").append(s.getName()).append(" ").append(s.getShape()).append("\n");
+      appendMotions(built, s);
+
     }
-    writeToDestination(this.outputDestination,
-        "canvas " + x + " " + y + " " + w + " " + h + "\n" + built.toString()
-            .substring(0, built.toString().length() - 1));
+    writeToDestination(this.outputDestination, built.toString()
+        .substring(0, built.toString().length() - 1).replaceAll("\\.0", ""));
   }
 
-  @Override
-  public void setOutputDestination(Appendable outArg) {
-    this.outputDestination = outArg;
+  /**
+   * Appends the Shape's motions to the StringBuilder passed in.
+   *
+   * @param built The StringBuilder to have motions appended to it.
+   * @param s The Shape whose Motions will be appended as Strings.
+   */
+  private void appendMotions(StringBuilder built, Shape s) {
+    for (Motion m : s.getMotions()) {
+      built.append("motion ").append(s.getName()).append(" ").append(m.getStartFrame().getTick())
+          .append(" ").append(m.getStartFrame().getFirstX()).append(" ")
+          .append(m.getStartFrame().getFirstY()).append(" ")
+          .append(m.getStartFrame().getFirstWidth()).append(" ")
+          .append(m.getStartFrame().getFirstHeight()).append(" ")
+          .append(m.getStartFrame().getFirstColors().replaceAll(",", " ")).append(" ")
+          .append(m.getEndFrame().getTick()).append(" ")
+          .append(m.getEndFrame().getFirstX()).append(" ").append(m.getEndFrame().getFirstY())
+          .append(" ").append(m.getEndFrame().getFirstWidth()).append(" ")
+          .append(m.getEndFrame().getFirstHeight()).append(" ")
+          .append(m.getEndFrame().getFirstColors().replaceAll(",", " ")).append("\n");
+    }
   }
 }
